5cc58e8c58332887d479e597699cb41144f655c0
Marc-Antoine FERNANDES <embraser01@gmail.com>
2016-12-09 15:57:59 +0100

    Made Response

--- src/http/Response.java
+++ src/http/Response.java
@@ -1,7 +1,45 @@
 package http;
 
-/**
- * Created by marca on 09/12/2016.
- */
+import java.util.HashMap;
+import java.util.Map;
+
 public class Response {
+
+
+    private int statusCode;
+    private String body;
+    private HashMap<String, String> headers;
+
+
+    public Response() {
+        this(200);
+    }
+
+    public Response(int statusCode) {
+        this.statusCode = statusCode;
+        body = "";
+        headers = new HashMap<>();
+    }
+
+    public void appendHeader(String key, String value) {
+        headers.put(key, value);
+    }
+
+    public void appendBody(String part) {
+        body += part;
+    }
+
+    @Override
+    public String toString() {
+        String res = "HTTP/1.0 " + statusCode + " " + statusCode + "\r\n";
+
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+            res += entry.getKey() + ": " + entry.getValue() + "\r\n";
+        }
+
+        res += "\r\n";
+        res += body.isEmpty() ? "Default Web Page" : body;
+
+        return res;
+    }
 }
